9b21182fba36aa2f9c8d154bddf59a4fa368abae
HaarisMuneer <hom9@cornell.edu>
2015-11-18 09:58:14 -0500

    add xcode project

---
+++ VinylMap/VinylMap/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VinylMap
+//
+//  Created by Haaris Muneer on 11/18/15.
+//  Copyright © 2015 Toaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
