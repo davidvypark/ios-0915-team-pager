c047804c7f6376d7c342048a9a81d4be03168d76
HaarisMuneer <hom9@cornell.edu>
2015-11-18 11:36:54 -0500

    adds constants file for discogs consumer key and secret

---
+++ VinylMap/VinylMap/Constants.m
@@ -0,0 +1,17 @@
+//
+//  Constants.m
+//  
+//
+//  Created by Haaris Muneer on 11/18/15.
+//
+//
+
+#import "Constants.h"
+
+@implementation Constants
+
+NSString *const DiscogsConsumerKey = @"SzxEYziBhFGjxhpAifWT";
+NSString *const DiscogsConsumerSecret = @"HqulBARQtAVtlKVlvhWkAdhxAGmOMjcE";
+
+@end
+
