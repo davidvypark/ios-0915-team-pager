71cb7c90c1aa1c94bb0e680cbb41d6980921d64b
HaarisMuneer <hom9@cornell.edu>
2015-11-19 17:16:18 -0500

    added classes for collection views

---
+++ VinylMap/AlbumCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  AlbumCollectionViewCell.m
+//  VinylMap
+//
+//  Created by Haaris Muneer on 11/19/15.
+//  Copyright © 2015 Toaster. All rights reserved.
+//
+
+#import "AlbumCollectionViewCell.h"
+
+@implementation AlbumCollectionViewCell
+
+@end
