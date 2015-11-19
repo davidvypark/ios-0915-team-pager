f66db90137a323e79b68af9787350cea3fd355e9
HaarisMuneer <hom9@cornell.edu>
2015-11-19 17:30:31 -0500

    adds even more necessary classes for the collection screen

---
+++ VinylMap/MyAlbumsViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyAlbumsViewController.h
+//  VinylMap
+//
+//  Created by Haaris Muneer on 11/19/15.
+//  Copyright © 2015 Toaster. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyAlbumsViewController : UIViewController
+
+@end
